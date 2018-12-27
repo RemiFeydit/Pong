2d125eb82bc763ab4d89f475ddd2bb595f391674
Unknown <remi.feydit@ynov.com>
2018-12-27 01:39:21 +0100

    Mouvement balle perpétuel + gestion collision
    Modification code pour faire bouger les balles tout le temps et gestion collision contre les murs

--- pong.py
+++ pong.py
@@ -1,9 +1,30 @@
-from tkinter import *
+from tkinter import*
+import time
 
- 
-menu=Tk()
+
+menu = Tk()
  
 canvas = Canvas(menu, width= 800, height=600, bg='black')
-
 canvas.pack()
+
+
+
+ball = canvas.create_oval(390,290,410,310, fill = "red")
+
+def ball_move(x,y):
+    while True :
+        canvas.move(ball,x,y)
+        menu.update()
+        time.sleep(.025)
+        pos = canvas.coords(ball)
+        if pos[3] >= 600 or pos[1] <= 0 :
+            y = -y
+        if pos[2] >= 800 or pos[0] <= 0:
+            x = -x
+   
+ball_move(10,10)
+
+
+
+
 menu.mainloop()
